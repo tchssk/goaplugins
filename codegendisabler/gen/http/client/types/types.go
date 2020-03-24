cce8fde87b992c912dd881aa8f1c2e699817dccd
Taichi Sasaki <tchsskk@gmail.com>
2020-03-25 01:11:16 +0900

    Add codegendisabler for gen/http/client/types

---
+++ codegendisabler/gen/http/client/types/types.go
@@ -0,0 +1,12 @@
+package types
+
+import (
+	_ "github.com/tchssk/goaplugins/codegendisabler/gen/http/client/types/client_body_attributes"
+	_ "github.com/tchssk/goaplugins/codegendisabler/gen/http/client/types/client_body_init"
+	_ "github.com/tchssk/goaplugins/codegendisabler/gen/http/client/types/client_error_body"
+	_ "github.com/tchssk/goaplugins/codegendisabler/gen/http/client/types/client_error_result_init"
+	_ "github.com/tchssk/goaplugins/codegendisabler/gen/http/client/types/client_request_body"
+	_ "github.com/tchssk/goaplugins/codegendisabler/gen/http/client/types/client_response_body"
+	_ "github.com/tchssk/goaplugins/codegendisabler/gen/http/client/types/client_result_init"
+	_ "github.com/tchssk/goaplugins/codegendisabler/gen/http/client/types/client_validate"
+)
