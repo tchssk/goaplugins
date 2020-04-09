0bc5093dedeace25aeebebe06a9cf77a7b396f6b
Taichi Sasaki <tchsskk@gmail.com>
2020-04-09 21:49:02 +0900

    Add codegendisabler for client-init

---
+++ codegendisabler/gen/http/client/client/client_init/generate.go
@@ -0,0 +1,29 @@
+package clientinit
+
+import (
+	"path/filepath"
+	"strings"
+
+	"goa.design/goa/v3/codegen"
+	"goa.design/goa/v3/eval"
+)
+
+func init() {
+	codegen.RegisterPlugin("codegendisabler-gen-http-client-client-clientinit", "gen", nil, Generate)
+}
+
+func Generate(genpkg string, roots []eval.Root, files []*codegen.File) ([]*codegen.File, error) {
+	for _, f := range files {
+		if !strings.HasSuffix(f.Path, filepath.Join("client", "client.go")) {
+			continue
+		}
+		var sections []*codegen.SectionTemplate
+		for _, section := range f.SectionTemplates {
+			if section.Name != "client-init" {
+				sections = append(sections, section)
+			}
+		}
+		f.SectionTemplates = sections
+	}
+	return files, nil
+}
