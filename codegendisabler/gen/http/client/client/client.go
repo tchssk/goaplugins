28a35de88667d0aa9ebd954e295cce280023a56f
Taichi Sasaki <tchsskk@gmail.com>
2020-04-19 13:46:14 +0900

    Add codegendisabler for gen/http/client/client

---
+++ codegendisabler/gen/http/client/client/client.go
@@ -0,0 +1,8 @@
+package client
+
+import (
+	_ "github.com/tchssk/goaplugins/codegendisabler/gen/http/client/client/client_endpoint_init"
+	_ "github.com/tchssk/goaplugins/codegendisabler/gen/http/client/client/client_init"
+	_ "github.com/tchssk/goaplugins/codegendisabler/gen/http/client/client/client_struct"
+	_ "github.com/tchssk/goaplugins/codegendisabler/gen/http/client/client/multipart_request_encoder_type"
+)
