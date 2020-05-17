8f429c8981439ed2ff7687a471e105f88d5e44b1
Taichi Sasaki <tchsskk@gmail.com>
2020-05-17 18:49:51 +0900

    Add codegendisabler for gen/http/client/encode_decode

---
+++ codegendisabler/gen/http/client/encode_decode/encode_decode.go
@@ -0,0 +1,10 @@
+package paths
+
+import (
+	_ "github.com/tchssk/goaplugins/codegendisabler/gen/http/client/encode_decode/build_stream_request"
+	_ "github.com/tchssk/goaplugins/codegendisabler/gen/http/client/encode_decode/client_transform_helper"
+	_ "github.com/tchssk/goaplugins/codegendisabler/gen/http/client/encode_decode/multipart_request_encoder"
+	_ "github.com/tchssk/goaplugins/codegendisabler/gen/http/client/encode_decode/request_builder"
+	_ "github.com/tchssk/goaplugins/codegendisabler/gen/http/client/encode_decode/request_encoder"
+	_ "github.com/tchssk/goaplugins/codegendisabler/gen/http/client/encode_decode/response_decoder"
+)
