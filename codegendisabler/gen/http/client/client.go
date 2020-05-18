065e96f5c49433daf742ac8423da4f5dad1beebe
Taichi Sasaki <tchsskk@gmail.com>
2020-05-18 22:10:18 +0900

    Add codegendisabler for gen/http/client

---
+++ codegendisabler/gen/http/client/client.go
@@ -0,0 +1,9 @@
+package client
+
+import (
+	_ "github.com/tchssk/goaplugins/codegendisabler/gen/http/client/cli"
+	_ "github.com/tchssk/goaplugins/codegendisabler/gen/http/client/client"
+	_ "github.com/tchssk/goaplugins/codegendisabler/gen/http/client/encode_decode"
+	_ "github.com/tchssk/goaplugins/codegendisabler/gen/http/client/paths"
+	_ "github.com/tchssk/goaplugins/codegendisabler/gen/http/client/types"
+)
