068e64e685c1d5842ad63774d778e07aab89cad6
Taichi Sasaki <tchsskk@gmail.com>
2020-05-18 23:19:17 +0900

    Fix codegendisabler for gen/http/client/cli

--- codegendisabler/gen/http/client/cli/cli.go
+++ codegendisabler/gen/http/client/cli/cli.go
@@ -15,7 +15,7 @@ func init() {
 func Generate(genpkg string, roots []eval.Root, files []*codegen.File) ([]*codegen.File, error) {
 	var fs []*codegen.File
 	for _, f := range files {
-		if strings.HasSuffix(f.Path, filepath.Join("client", "cli.go")) {
+		if strings.HasPrefix(f.Path, filepath.Join("gen", "http")) && strings.HasSuffix(f.Path, filepath.Join("client", "cli.go")) {
 			continue
 		}
 		fs = append(fs, f)
