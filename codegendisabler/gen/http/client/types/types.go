7975276367986386f2eb7e664066aeaca71330ba
Taichi Sasaki <tchsskk@gmail.com>
2020-05-18 22:30:51 +0900

    Fix codegendisabler for gen/http/client/types

--- codegendisabler/gen/http/client/types/types.go
+++ codegendisabler/gen/http/client/types/types.go
@@ -1,12 +1,24 @@
 package types
 
 import (
-	_ "github.com/tchssk/goaplugins/codegendisabler/gen/http/client/types/client_body_attributes"
-	_ "github.com/tchssk/goaplugins/codegendisabler/gen/http/client/types/client_body_init"
-	_ "github.com/tchssk/goaplugins/codegendisabler/gen/http/client/types/client_error_body"
-	_ "github.com/tchssk/goaplugins/codegendisabler/gen/http/client/types/client_error_result_init"
-	_ "github.com/tchssk/goaplugins/codegendisabler/gen/http/client/types/client_request_body"
-	_ "github.com/tchssk/goaplugins/codegendisabler/gen/http/client/types/client_response_body"
-	_ "github.com/tchssk/goaplugins/codegendisabler/gen/http/client/types/client_result_init"
-	_ "github.com/tchssk/goaplugins/codegendisabler/gen/http/client/types/client_validate"
+	"path/filepath"
+	"strings"
+
+	"goa.design/goa/v3/codegen"
+	"goa.design/goa/v3/eval"
 )
+
+func init() {
+	codegen.RegisterPlugin("codegendisabler-gen-http-client-types", "gen", nil, Generate)
+}
+
+func Generate(genpkg string, roots []eval.Root, files []*codegen.File) ([]*codegen.File, error) {
+	var fs []*codegen.File
+	for _, f := range files {
+		if strings.HasSuffix(f.Path, filepath.Join("client", "types.go")) {
+			continue
+		}
+		fs = append(fs, f)
+	}
+	return fs, nil
+}
