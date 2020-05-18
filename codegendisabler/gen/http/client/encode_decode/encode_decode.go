3e55c0f949f6e47f86a88ef710fa2b3e5476e7f2
Taichi Sasaki <tchsskk@gmail.com>
2020-05-18 22:28:55 +0900

    Fix codegendisabler for gen/http/client/encode_decode

--- codegendisabler/gen/http/client/encode_decode/encode_decode.go
+++ codegendisabler/gen/http/client/encode_decode/encode_decode.go
@@ -1,10 +1,24 @@
-package paths
+package encodedecode
 
 import (
-	_ "github.com/tchssk/goaplugins/codegendisabler/gen/http/client/encode_decode/build_stream_request"
-	_ "github.com/tchssk/goaplugins/codegendisabler/gen/http/client/encode_decode/client_transform_helper"
-	_ "github.com/tchssk/goaplugins/codegendisabler/gen/http/client/encode_decode/multipart_request_encoder"
-	_ "github.com/tchssk/goaplugins/codegendisabler/gen/http/client/encode_decode/request_builder"
-	_ "github.com/tchssk/goaplugins/codegendisabler/gen/http/client/encode_decode/request_encoder"
-	_ "github.com/tchssk/goaplugins/codegendisabler/gen/http/client/encode_decode/response_decoder"
+	"path/filepath"
+	"strings"
+
+	"goa.design/goa/v3/codegen"
+	"goa.design/goa/v3/eval"
 )
+
+func init() {
+	codegen.RegisterPlugin("codegendisabler-gen-http-client-encode-decode", "gen", nil, Generate)
+}
+
+func Generate(genpkg string, roots []eval.Root, files []*codegen.File) ([]*codegen.File, error) {
+	var fs []*codegen.File
+	for _, f := range files {
+		if strings.HasSuffix(f.Path, filepath.Join("client", "encode_decode.go")) {
+			continue
+		}
+		fs = append(fs, f)
+	}
+	return fs, nil
+}
