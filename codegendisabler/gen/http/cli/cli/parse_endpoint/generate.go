37dbc0214e032a4a51cb64d70dfa8aa7d10a24cc
Taichi Sasaki <tchsskk@gmail.com>
2020-05-19 23:18:18 +0900

    Add codegendisabler for parse-endpoint

---
+++ codegendisabler/gen/http/cli/cli/parse_endpoint/generate.go
@@ -0,0 +1,29 @@
+package parseendpoint
+
+import (
+	"path/filepath"
+	"strings"
+
+	"goa.design/goa/v3/codegen"
+	"goa.design/goa/v3/eval"
+)
+
+func init() {
+	codegen.RegisterPlugin("codegendisabler-gen-http-cli-cli-parseendpoint", "gen", nil, Generate)
+}
+
+func Generate(genpkg string, roots []eval.Root, files []*codegen.File) ([]*codegen.File, error) {
+	for _, f := range files {
+		if strings.HasPrefix(f.Path, filepath.Join("gen", "http", "cli")) && strings.HasSuffix(f.Path, "cli.go") {
+			var sections []*codegen.SectionTemplate
+			for _, section := range f.SectionTemplates {
+				if section.Name == "parse-endpoint" {
+					continue
+				}
+				sections = append(sections, section)
+			}
+			f.SectionTemplates = sections
+		}
+	}
+	return files, nil
+}
