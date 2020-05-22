099d72c1de7afbe0198dd0253c3ec31a2b8b0120
Taichi Sasaki <tchsskk@gmail.com>
2020-05-22 19:14:13 +0900

    Fix codegendisabler for paths

--- codegendisabler/gen/http/client/paths/path/generate.go
+++ codegendisabler/gen/http/client/paths/path/generate.go
@@ -14,7 +14,7 @@ func init() {
 
 func Generate(genpkg string, roots []eval.Root, files []*codegen.File) ([]*codegen.File, error) {
 	for _, f := range files {
-		if !strings.HasSuffix(f.Path, filepath.Join("client", "paths.go")) {
+		if !strings.HasPrefix(f.Path, filepath.Join("gen", "http")) || !strings.HasSuffix(f.Path, filepath.Join("client", "paths.go")) {
 			continue
 		}
 		var sections []*codegen.SectionTemplate
