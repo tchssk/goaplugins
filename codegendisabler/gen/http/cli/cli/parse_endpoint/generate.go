085333baf3a45fe9aece2f173d24aabbb37cda37
Taichi Sasaki <tchsskk@gmail.com>
2020-05-26 00:09:07 +0900

    Fix codegendisabler for parse-endpoint

--- codegendisabler/gen/http/cli/cli/parse_endpoint/generate.go
+++ codegendisabler/gen/http/cli/cli/parse_endpoint/generate.go
@@ -14,16 +14,16 @@ func init() {
 
 func Generate(genpkg string, roots []eval.Root, files []*codegen.File) ([]*codegen.File, error) {
 	for _, f := range files {
-		if strings.HasPrefix(f.Path, filepath.Join("gen", "http", "cli")) && strings.HasSuffix(f.Path, "cli.go") {
-			var sections []*codegen.SectionTemplate
-			for _, section := range f.SectionTemplates {
-				if section.Name == "parse-endpoint" {
-					continue
-				}
+		if !strings.HasPrefix(f.Path, filepath.Join("gen", "http", "cli")) || !strings.HasSuffix(f.Path, "cli.go") {
+			continue
+		}
+		var sections []*codegen.SectionTemplate
+		for _, section := range f.SectionTemplates {
+			if section.Name != "parse-endpoint" {
 				sections = append(sections, section)
 			}
-			f.SectionTemplates = sections
 		}
+		f.SectionTemplates = sections
 	}
 	return files, nil
 }
