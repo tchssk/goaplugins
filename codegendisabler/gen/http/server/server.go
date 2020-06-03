d2159870986041e3c6bbbfe78516fd409a359fc4
Taichi Sasaki <tchsskk@gmail.com>
2020-06-03 19:36:09 +0900

    Add codegendisabler for gen/http/server

---
+++ codegendisabler/gen/http/server/server.go
@@ -0,0 +1,8 @@
+package server
+
+import (
+	_ "github.com/tchssk/goaplugins/codegendisabler/gen/http/server/encode_decode"
+	_ "github.com/tchssk/goaplugins/codegendisabler/gen/http/server/paths"
+	_ "github.com/tchssk/goaplugins/codegendisabler/gen/http/server/server"
+	_ "github.com/tchssk/goaplugins/codegendisabler/gen/http/server/types"
+)
