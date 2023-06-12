7ebc8b56b28ac2a14df460c8938be8afbb309ec1
Taichi Sasaki <tchsskk@gmail.com>
2023-06-12 21:57:26 +0900

    Add codegendisabler for client-websocket-struct-type

---
+++ codegendisabler/gen/http/client/websocket/client_websocket_struct_type/generate.go
@@ -0,0 +1,29 @@
+package clientwebsocketstructtype
+
+import (
+	"path/filepath"
+	"strings"
+
+	"goa.design/goa/v3/codegen"
+	"goa.design/goa/v3/eval"
+)
+
+func init() {
+	codegen.RegisterPlugin("codegendisabler-gen-http-client-websocket-clientwebsocketstructtype", "gen", nil, Generate)
+}
+
+func Generate(genpkg string, roots []eval.Root, files []*codegen.File) ([]*codegen.File, error) {
+	for _, f := range files {
+		if !strings.HasPrefix(f.Path, filepath.Join("gen", "http")) || !strings.HasSuffix(f.Path, filepath.Join("client", "websocket.go")) {
+			continue
+		}
+		var sections []*codegen.SectionTemplate
+		for _, section := range f.SectionTemplates {
+			if section.Name != "client-websocket-struct-type" {
+				sections = append(sections, section)
+			}
+		}
+		f.SectionTemplates = sections
+	}
+	return files, nil
+}
