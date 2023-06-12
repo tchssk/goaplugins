a0510b418bde167126612928962409e5525c0c68
Taichi Sasaki <tchsskk@gmail.com>
2023-06-12 21:59:37 +0900

    Add codegendisabler for client-websocket-send

---
+++ codegendisabler/gen/http/client/websocket/client_websocket_send/generate.go
@@ -0,0 +1,29 @@
+package clientwebsocketsend
+
+import (
+	"path/filepath"
+	"strings"
+
+	"goa.design/goa/v3/codegen"
+	"goa.design/goa/v3/eval"
+)
+
+func init() {
+	codegen.RegisterPlugin("codegendisabler-gen-http-client-websocket-clientwebsocketsend", "gen", nil, Generate)
+}
+
+func Generate(genpkg string, roots []eval.Root, files []*codegen.File) ([]*codegen.File, error) {
+	for _, f := range files {
+		if !strings.HasPrefix(f.Path, filepath.Join("gen", "http")) || !strings.HasSuffix(f.Path, filepath.Join("client", "websocket.go")) {
+			continue
+		}
+		var sections []*codegen.SectionTemplate
+		for _, section := range f.SectionTemplates {
+			if section.Name != "client-websocket-send" {
+				sections = append(sections, section)
+			}
+		}
+		f.SectionTemplates = sections
+	}
+	return files, nil
+}
