a298181bef94bf89eac7d1091a07eee57b9ebf0a
Taichi Sasaki <tchsskk@gmail.com>
2023-06-17 22:15:04 +0900

    Add codegendisabler for server-use

---
+++ codegendisabler/gen/http/server/server/server_use/generate.go
@@ -0,0 +1,29 @@
+package serveruse
+
+import (
+	"path/filepath"
+	"strings"
+
+	"goa.design/goa/v3/codegen"
+	"goa.design/goa/v3/eval"
+)
+
+func init() {
+	codegen.RegisterPlugin("codegendisabler-gen-http-server-server-serveruse", "gen", nil, Generate)
+}
+
+func Generate(genpkg string, roots []eval.Root, files []*codegen.File) ([]*codegen.File, error) {
+	for _, f := range files {
+		if !strings.HasPrefix(f.Path, filepath.Join("gen", "http")) || !strings.HasSuffix(f.Path, filepath.Join("server", "server.go")) {
+			continue
+		}
+		var sections []*codegen.SectionTemplate
+		for _, section := range f.SectionTemplates {
+			if section.Name != "server-use" {
+				sections = append(sections, section)
+			}
+		}
+		f.SectionTemplates = sections
+	}
+	return files, nil
+}
